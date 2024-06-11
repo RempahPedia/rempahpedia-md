dc2151491d726e7901b0c113096fdc34e382bd19
stsrambe <stsrambe@gmail.com>
2024-06-11 17:08:06 +0700

    merge analyze button to cameraactivity

--- app/src/main/java/com/rempahpedia/rempahpedia/bottomnav/home/HomeFragment.kt
+++ app/src/main/java/com/rempahpedia/rempahpedia/bottomnav/home/HomeFragment.kt
@@ -1,24 +1,41 @@
 package com.rempahpedia.rempahpedia.bottomnav.home
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.rempahpedia.rempahpedia.R
 import com.rempahpedia.rempahpedia.databinding.FragmentHomeBinding
 import com.rempahpedia.rempahpedia.listspices.SpicesActivity
+import com.rempahpedia.rempahpedia.ui.classification.CameraActivity
+import com.rempahpedia.rempahpedia.ui.classification.ResultActivity
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private val cameraResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val imageUri = Uri.parse(data?.getStringExtra(CameraActivity.EXTRA_CAMERAX_IMAGE))
+            val intent = Intent(activity, ResultActivity::class.java)
+            intent.putExtra(ResultActivity.IMAGE_URI, imageUri.toString())
+            startActivity(intent)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -28,12 +45,12 @@ class HomeFragment : Fragment() {
 
         val btnAnalyze = binding.btnAnalyze
         val btnExplore = binding.btnExplore
-        val rempahFirst = binding.rempahFirst
-        val rempahSecond = binding.rempahSecond
+//        val rempahFirst = binding.rempahFirst
+//        val rempahSecond = binding.rempahSecond
 
         btnAnalyze.setOnClickListener {
-
-
+            val intent = Intent(activity, CameraActivity::class.java)
+            cameraResultLauncher.launch(intent)
         }
 
         btnExplore.setOnClickListener {
--- app/src/main/java/com/rempahpedia/rempahpedia/data/data/Spices.kt
+++ app/src/main/java/com/rempahpedia/rempahpedia/data/data/Spices.kt
@@ -2,7 +2,6 @@ package com.rempahpedia.rempahpedia.data.data
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-
 @Parcelize
 data class Spices(
     val name: String,
