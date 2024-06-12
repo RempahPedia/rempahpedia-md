465748d5dbfb67b779d927c55a6bf059082caeef
Alvin Xavier Rakha Wardhana <alvin.xavier@ui.ac.id>
2024-06-12 21:30:06 +0700

    refactor: removed unnecessary import

--- app/src/main/java/com/rempahpedia/rempahpedia/ui/MainActivity.kt
+++ app/src/main/java/com/rempahpedia/rempahpedia/ui/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.rempahpedia.rempahpedia.ui
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.rempahpedia.rempahpedia.R
 import com.rempahpedia.rempahpedia.bottomnav.home.HomeFragment
 import com.rempahpedia.rempahpedia.bottomnav.profile.ProfileFragment
 import com.rempahpedia.rempahpedia.databinding.ActivityMainBinding
 import com.rempahpedia.rempahpedia.ui.auth.AuthViewModel
 import com.rempahpedia.rempahpedia.ui.auth.AuthViewModelFactory
-import com.rempahpedia.rempahpedia.ui.classification.CameraActivity
-import com.rempahpedia.rempahpedia.ui.classification.CameraActivity.Companion.CAMERAX_RESULT
-import com.rempahpedia.rempahpedia.ui.classification.ResultActivity
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
@@ -50,12 +42,14 @@ class MainActivity : AppCompatActivity() {
                         .commit()
                     true
                 }
+
                 R.id.profile -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.main_frame, ProfileFragment())
                         .commit()
                     true
                 }
+
                 else -> false
             }
         }
